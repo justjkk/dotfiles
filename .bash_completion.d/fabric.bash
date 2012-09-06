153215ec55eb6a0982976a88793fd7d8844f2d1f
J Kishore kumar <justjkk@gmail.com>
2012-09-06 15:12:08 +0530

    Added autocomplete for fabric and symfony console

---
+++ .bash_completion.d/fabric.bash
@@ -0,0 +1,16 @@
+#
+# Bash completion for fabric
+#
+function _fab_complete() {
+    local cur
+    if [[ -f "fabfile.py" || -d "fabfile" ]]; then
+        cur="${COMP_WORDS[COMP_CWORD]}"
+        COMPREPLY=( $(compgen -W "$(fab -F short -l)" -- ${cur}) )
+        return 0
+    else
+        # no fabfile.py found. Don't do anything.
+        return 1
+    fi
+}
+
+complete -o nospace -F _fab_complete fab
