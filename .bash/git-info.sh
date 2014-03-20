3a591427deb9129db553ce816e1bb3887482311c
J Kishore kumar <justjkk@gmail.com>
2014-03-20 18:37:33 +0530

    git-info works with git 1.8 and higher

--- .bash/git-info.sh
+++ .bash/git-info.sh
@@ -4,8 +4,22 @@ green=$(tput setaf 2)
 blue=$(tput setaf 4)
 bold=$(tput bold)
 reset=$(tput sgr0)
+function _ver_higher {
+  ver=`echo -ne "$1\n$2" |sort -Vr |head -n1`
+  if [ "$2" == "$1" ]; then
+    return 1
+  elif [ "$2" == "$ver" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
 function parse_git_dirty {
-  [[ $(git status 2> /dev/null | tail -n1) != 'nothing to commit (working directory clean)' ]] && echo $red || echo $green
+  if _ver_higher $(git version | cut -d' ' -f3) '1.8'; then
+    [[ $(git status 2> /dev/null | tail -n1) != 'nothing to commit (working directory clean)' ]] && echo $red || echo $green
+  else
+    [[ $(git status 2> /dev/null | tail -n1) != 'nothing to commit, working directory clean' ]] && echo $red || echo $green
+  fi
 }
 function parse_git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/\* \(.*\)/\1/"
